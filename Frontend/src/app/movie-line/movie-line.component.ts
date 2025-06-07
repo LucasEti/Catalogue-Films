48e1f152b5c1284bd77a2a56ff620c42475402fe
l0etie <130457558+l0etie@users.noreply.github.com>
2025-06-07 18:21:57 -0400

    Convertir Frontend en dossier normal (suppression du submodule)

---
+++ Frontend/src/app/movie-line/movie-line.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-line',
+  imports: [],
+  templateUrl: './movie-line.component.html',
+  styleUrl: './movie-line.component.scss'
+})
+export class MovieLineComponent {
+
+}
